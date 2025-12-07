f41b1c6dda00b3e0c1e80391786581b94fdfc163
Ashley Williams <ashleyw@beigetech.com.au>
2025-12-07 11:27:41 +1000

    added print classes and com.example package

---
+++ step_4/Main.java
@@ -0,0 +1,8 @@
+import com.example.*;
+class Main {
+
+  public static void main(String[] args) {
+    Print p = new Print();
+    p.doPrint("Test String");
+  }
+}
---
+++ step_4/com/example/Print.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Print {
+
+  public Print() {}
+
+  public void doPrint(String stringToPrint) {
+    System.out.println(stringToPrint);
+  }
+}
